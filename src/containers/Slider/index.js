bcbe34528182ba3f462e5577ff2488c689d82c3f
SeelyksDev <kevincordier.dev@gmail.com>
2024-03-31 18:16:12 +0200

    Réparation des paginations du slider

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -48,7 +48,7 @@ const Slider = () => {
                                     key={`${event.id}`}
                                     type="radio"
                                     name="radio-button"
-                                    checked={idx === radioIdx}
+                                    checked={index === radioIdx}
                                 />
                             ))}
                         </div>
