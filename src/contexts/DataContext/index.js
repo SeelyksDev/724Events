d2854d01ce8af13efc9e1629b13b748072d240d1
SeelyksDev <kevincordier.dev@gmail.com>
2024-04-01 19:29:57 +0200

    fix card footer event

--- src/contexts/DataContext/index.js
+++ src/contexts/DataContext/index.js
@@ -1,52 +1,65 @@
 import PropTypes from "prop-types";
 import {
-  createContext,
-  useCallback,
-  useContext,
-  useEffect,
-  useState,
+    createContext,
+    useCallback,
+    useContext,
+    useEffect,
+    useState,
 } from "react";
 
 const DataContext = createContext({});
 
 export const api = {
-  loadData: async () => {
-    const json = await fetch("/events.json");
-    return json.json();
-  },
+    loadData: async () => {
+        const json = await fetch("/events.json");
+        return json.json();
+    },
+};
+
+const getLastEvent = (data) => {
+    const variable = data?.events.sort((evtA, evtB) =>
+        new Date(evtA.date) > new Date(evtB.date) ? -1 : 1
+    );
+    const lastEvent = variable[0];
+
+    return lastEvent;
 };
 
 export const DataProvider = ({ children }) => {
-  const [error, setError] = useState(null);
-  const [data, setData] = useState(null);
-  const getData = useCallback(async () => {
-    try {
-      setData(await api.loadData());
-    } catch (err) {
-      setError(err);
-    }
-  }, []);
-  useEffect(() => {
-    if (data) return;
-    getData();
-  });
-  
-  return (
-    <DataContext.Provider
-      // eslint-disable-next-line react/jsx-no-constructed-context-values
-      value={{
-        data,
-        error,
-      }}
-    >
-      {children}
-    </DataContext.Provider>
-  );
+    const [error, setError] = useState(null);
+    const [data, setData] = useState(null);
+    const [last, setLast] = useState(null);
+    const getData = useCallback(async () => {
+        try {
+            const loadedData = await api.loadData();
+            setData(loadedData);
+            setLast(getLastEvent(loadedData));
+        } catch (err) {
+            setError(err);
+        }
+    }, []);
+    useEffect(() => {
+        if (data) return;
+        getData();
+    });
+
+    return (
+        <DataContext.Provider
+            // eslint-disable-next-line react/jsx-no-constructed-context-values
+            value={{
+                data,
+                error,
+                last,
+            }}
+        >
+            {children}
+        </DataContext.Provider>
+    );
 };
 
 DataProvider.propTypes = {
-  children: PropTypes.node.isRequired,
-}
+    children: PropTypes.node.isRequired,
+};
 
 export const useData = () => useContext(DataContext);
 
